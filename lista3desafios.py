9b86610aaf1cd565fa16846a794d0ae1c46c03a2
sheilagomes <sheilagomes1@gmail.com>
2020-10-21 08:46:16 -0300

    Adicionados os exercicios de arquivos e dicionários

--- lista3desafios.py
+++ lista3desafios.py
@@ -1,13 +1,9 @@
-#4. Dado um número inteiro positivo, determine a sua decomposição em fatores primos
-#calculando também a multiplicidade de cada fator.
-
 #1. Dizemos que um número natural é triangular se ele é produto de três números naturais
 #consecutivos. Exemplo: 120 é triangular, pois 4.5.6 = 120. Dado um inteiro não-negativo n,
 #verificar se n é triangular.
 tri = int(input('Digite um número inteiro: '))
 a, b, c, t = 1, 2 , 3, False
 while a * b * c <= tri:
-    print(f'{a} * {b} * {c} = {a * b * c}')
     if a * b * c == tri:
         t = True
     a, b, c = a + 1, b + 1, c + 1
@@ -15,6 +11,14 @@ if t:
     print(f'O número {tri} é tringular')
 else:
     print(f'O número {tri} não é tringular')
+
+#1 - versão zumbi
+tri = int(input('Digite um número inteiro: '))
+k = 1
+while k * (k + 1) * (k + 2) < tri:
+    k = k + 1
+print(k * (k + 1) * (k + 2) == tri)
+    
 
 #2. Indique como um troco deve ser dado utilizando-se um número mínimo de notas. Seu
 #algoritmo deve ler o valor da conta a ser paga e o valor do pagamento efetuado desprezando
@@ -48,6 +52,16 @@ if troco >= 0:
 
 print(f'O troco de R$ {pago - conta:.2f} equivale a:\n{cinquenta} nota(s) de cinquenta\n{vinte} nota(s) de vinte\n{dez} nota(s) de dez\n{cinco} nota(s) de cinco\n{dois} nota(s) de dois\n{um} nota(s) de um')
 
+#2 - versão zumbi
+conta = int(input('Digite o valor da conta: '))
+pago = int(input('Digite o valor pago: '))
+troco = pago - conta
+notas = [50, 20, 10, 5, 2, 1]
+for nota in notas:
+    while troco >= nota:
+    print(f'Uma nota de {nota}')
+    troco -= nota
+
 #3. Verifique se um inteiro positivo n é primo.
 numero = int(input('Digite um número inteiro positivo: '))
 cont, primo = 2, True
@@ -60,6 +74,24 @@ if (primo):
 else:
     print(f'O número {numero} não é primo')
 
+#3 - versão zumbi
+numero = int(input('Digite um número inteiro positivo: '))
+cont, divisores = 1, 0
+while cont <= numero:
+    if numero % cont == 0:
+        divisores = divisores + 1
+    cont = cont + 1
+print(divisores == 2)
+
+#4. Dado um número inteiro positivo, determine a sua decomposição em fatores primos
+#calculando também a multiplicidade de cada fator.
+# versão zumbi
+n = int(input('Número: '))
+for k in range (2, n):
+    while n % k == 0:
+        print(k)
+        n = n / k
+        
 #5. Faça um programa que peça um inteiro positivo e o mostre invertido. Ex.: 1234 gera 4321
 numero = input('Digite um numero inteiro positivo: ')
 x = len(numero)-1
@@ -68,3 +100,17 @@ while x >= 0:
     novo = novo + numero[x]
     x = x - 1
 print(novo)
+
+#5 - versão zumbi 1
+n = input('Número: ')
+n  = n[::-1]
+print(n)
+
+#5 - versão zumbi 2
+n = input('Número: ')
+inv = 0
+while n > 0:
+    inv *= 10
+    inv += n % 10
+    n //= 10
+print(inv)
--- lista4.py
+++ lista4.py
@@ -1,12 +1,55 @@
 #1. Sorteie 10 inteiros entre 1 e 100 para uma lista e descubra o maior e o menor valor, sem usar
 #as funções max e min.
+from random import sample
+lista = sample(range(100),10)
+maior = menor = lista[0]
+for i in range(10):
+    if lista[i] >= maior: maior = lista[i]
+    if lista[i] <= menor: menor = lista[i]
+print(lista)
+print(f'O maior número da lista é {maior} e o menor é {menor}')
 
 #2. Sorteie 20 inteiros entre 1 e 100 numa lista. Armazene os números pares na lista PAR e os
 #números ímpares na lista IMPAR. Imprima as três listas.
+from random import sample
+lista = sample(range(100),20)
+PAR, IMPAR = [],[]
+for i in range(20):
+    if lista[i] % 2 == 0: PAR.append(lista[i])
+    else: IMPAR.append(lista[i])
+print(lista)
+print(PAR)
+print(IMPAR)
+
+#2 - versão zumbi com list comprehension
+from random import sample
+lista = sample(range(100),20)
+PAR = [x for x in lista if x % 2 == 0]
+IMPAR = [x for x in lista if x % 2 == 1]
+kprint(lista)
+print(PAR)
+print(IMPAR)
 
 #3. Faça um programa que crie dois vetores com 10 elementos aleatórios entre 1 e 100. Gere um
 #terceiro vetor de 20 elementos, cujos valores deverão ser compostos pelos elementos
 #intercalados dos dois outros vetores. Imprima os três vetores.
+from random import sample
+lista1, lista2, lista3 = sample(range(100),10), sample(range(100),10), []
+for i in range(10):
+    lista3.append(lista1[i])
+    lista3.append(lista2[i])
+print(lista1)
+print(lista2)
+print(lista3)
+
+#3 - versão zumbi
+from random import sample
+lista1, lista2, lista3 = sample(range(100),10), sample(range(100),10), []
+for i in zip(lista1, lista2):
+    lista3.extend(list(i))
+print(lista1)
+print(lista2)
+print(lista3)
 
 #4. Seja o statement sobre diversidade: “The Python Software Foundation and the global Python
 #community welcome and encourage participation by everyone. Our community is based on
@@ -16,7 +59,84 @@
 #split(), a seguir crie uma lista com as palavras que começam ou terminam com uma das letras
 #“python”. Imprima a lista resultante. Não se esqueça de remover antes os caracteres especiais
 #e cuidado com maiúsculas e minúsculas.
+lista1 ='''The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you.'''
+lista1, lista2 = lista1.lower().split(' '), []
+for i in range(len(lista1)):
+    if ',' in lista1[i] or '.' in lista1[i] or ':' in lista1[i]:
+        lista1[i] = lista1[i][:-1]
+    if lista1[i][0:1] in 'python' or lista1[i][-2:-1] in 'python':
+        lista2.append(lista1[i])
+print(lista1)
+print(lista2)
+
+#4 - versão zumbi - 1
+texto ='''The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you.'''.lower()
+import string
+for c in string.punctuation:
+    texto = texto.replace(c, ' ')
+resp = []
+for p in texto.split():
+    if p[0] in 'python' or p[-1] in 'python':
+        resp.append(p)
+print(resp)
+
+#4 - versão zumbi - 2 com list comprehension
+texto ='''The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you.'''.lower()
+import string
+for c in string.punctuation:
+    texto = texto.replace(c, ' ')
+resp = [p for po in texto.split()
+    if p[0] in 'python' or p[-1] in 'python']
+print(resp)
 
 #5. Seja o mesmo texto acima “splitado”. Calcule quantas palavras possuem uma das letras
 #“python” e que tenham mais de 4 caracteres. Não se esqueça de transformar maiúsculas para
 #minúsculas e de remover antes os caracteres especiais.
+lista1 ='''The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you.'''
+lista1, lista2, tem = lista1.lower().split(' '), [], False
+for i in range(len(lista1)):
+    if ',' in lista1[i] or '.' in lista1[i] or ':' in lista1[i]:
+        lista1[i] = lista1[i][:-1]
+    for k in range(len(lista1[i])):
+        if lista1[i][k] in 'python' and len(lista1[i]) > 4:
+            tem = True
+    if tem:
+        lista2.append(lista1[i])
+        tem = False
+print(lista2)    
+print(f'Existem {len(lista2)} palavras com uma das letras de "python" e mais de 4 caracteres')
+
+#5 - versão zumbi 1
+texto ='''The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you.'''.lower()
+import string
+for c in string.punctuation:
+    texto = texto.replace(c, ' ')
+
+def pitônica(palavra):
+    for letra in palavra:
+        if letra in 'python':
+            return True
+    return False
+
+resp = []
+for p in texto.split()
+if pitônica(p) and len(p) > 4:
+    resp.append(p)
+print(resp)
+
+#5 - versão zumbi 2
+texto ='''The Python Software Foundation and the global Python community welcome and encourage participation by everyone. Our community is based on mutual respect, tolerance, and encouragement, and we are working to help each other live up to these principles. We want our community to be more diverse: whoever you are, and whatever your background, we welcome you.'''.lower()
+import string
+for c in string.punctuation:
+    texto = texto.replace(c, ' ')
+
+def pitônica(palavra):
+    for letra in palavra:
+        if letra in 'python':
+            return True
+    return False
+
+resp = [p ofr p in texto.split()
+        if pitônica(p) and len(p) > 4]
+print(resp)
+print(len(resp))
